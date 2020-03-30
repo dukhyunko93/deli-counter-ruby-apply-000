6ac2d4105ba10368bea4dd10d03ac31a663f2bea
Duke Ko <dukhyunko93@gmail.com>
2020-03-30 13:36:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length(name)} in line."
 end
 
 def now_serving(katz_deli)
